c036cc3a2194dacbd5ce6e29a70e8263dedd69e0
Long <vanlong5436@gmail.com>
2023-10-28 02:09:31 +0700

    update method createUUID, update workflows

--- src/package/utils/generalMethods.ts
+++ src/package/utils/generalMethods.ts
@@ -1,14 +1,7 @@
 const defaultLength = 5; // length of uuid string
 
 const createUUID = (length?: number): string => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  let className = '';
-
-  for (let i = 0; i < (length || defaultLength); i++) {
-    className += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-
-  return className;
+  return '_' + (Math.random() + 1).toString(36).substring((length || defaultLength) - 1);
 };
 
 export { createUUID };
